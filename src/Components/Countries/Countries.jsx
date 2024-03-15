a215c8a149b3f23af713d44193c72b1b8ceff49b
sabbir347256 <srgamingzone71@gmail.com>
2024-03-15 08:33:03 +0600

    all is done

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -6,11 +6,17 @@ import './countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([]);
     const [visitCountries,setVisitCountries] = useState([]);
+    const [visitedFlags,setVisitedFlags] = useState([]);
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
             .then(data => setCountries(data))
     }, [])
+
+    const handleVisitedFlag =(flag) =>{
+        const newVisitedFlag = [...visitedFlags,flag];
+        setVisitedFlags(newVisitedFlag);
+    }
 
     const handleVisitedCountry = c =>{
         const newVisitedCountry = [...visitCountries,c];
@@ -30,10 +36,16 @@ const Countries = () => {
                     }
                 </ul>
             </div>
+            <div>
+                {
+                    visitedFlags.map((flag,idx) =><img key={idx} src={flag}></img>)
+                }
+            </div>
             <div className='countries'>
                 {
                     countries.map(country => <Country
                         handleVisitedCountry ={handleVisitedCountry}
+                        handleVisitedFlag ={handleVisitedFlag}
                         key={country.cca3} country={country}></Country>)
                 }
             </div>
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types'
 import './Country.css'
 import { useState } from 'react';
-const Country = ({ country,handleVisitedCountry}) => {
+const Country = ({ country,handleVisitedCountry,handleVisitedFlag} ) => {
     const { name, flags, population, area, cca3 } = country;
     const [visited, setVisited] = useState(false);
     const handleVisite = () => {
         setVisited(!visited);
     }
+    console.log(country)
     return (
         <div className={visited ?'visited' : 'country'}>
             <h3>Name : {name.common}</h3>
@@ -16,17 +17,21 @@ const Country = ({ country,handleVisitedCountry}) => {
             <p>Code : {cca3}</p>
             <button style={{marginBottom:10}} onClick={() => handleVisitedCountry(country)}>Mark as Visited</button>
             <br />
+            <button onClick={() =>handleVisitedFlag(country.flags.png)}>Add Flag</button>
+            <br />
             <button style={{marginBottom : 10}} onClick={handleVisite}>{visited ? 'Visited' : 'Going'}</button>
             <div>
                 {
                     visited ? 'All ready this country visited' : 'I will going this country'
                 }
             </div>
+            <hr />
         </div>
     );
 };
 Country.propTypes = {
     handleVisitedCountry :PropTypes.func.isRequired,
-    country: PropTypes.object.isRequired
+    country: PropTypes.object.isRequired,
+    handleVisitedFlag :PropTypes.func.isRequired
 }
 export default Country;
